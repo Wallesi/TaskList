4d239e2ca2bc3850da0572134a6e14f38683257b
Wallesi <waldemargalizzi98@gmail.com>
2022-10-25 17:20:49 -0300

    Tercer commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS Task[] =[
+    {
+        id:1,
+        text: "Terminar primer modulo de angular",
+        day: "Agosto 5 a las 12:00",
+        reminder:2,
+    },
+    {
+        id:2,
+        text: "Terminar primer modulo de angular",
+        day: "Agosto 5 a las 12:00",
+        reminder:true
+    },
+    {
+        id:3,
+        text: "Terminar primer modulo de angular",
+        day: "Agosto 5 a las 12:00",
+        reminder:false
+    },
+    {
+        id:3,
+        text: "Terminar primer modulo de angular",
+        day: "Agosto 5 a las 12:00",
+        reminder:false
+    }
+]
